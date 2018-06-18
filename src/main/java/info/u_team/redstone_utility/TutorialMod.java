a79cbb2894c10305c4ea918c7c8dbc24d6907dca
HyCraftHD <hycrafthd@live.de>
2018-06-18 22:29:40 +0200

    Base mod classes

---
+++ src/main/java/info/u_team/redstone_utility/TutorialMod.java
@@ -0,0 +1,38 @@
+package info.u_team.redstone_utility;
+
+import static info.u_team.redstone_utility.TutorialConstants.*;
+
+import info.u_team.redstone_utility.proxy.CommonProxy;
+import net.minecraftforge.fml.common.*;
+import net.minecraftforge.fml.common.Mod.*;
+import net.minecraftforge.fml.common.event.*;
+
+@Mod(modid = MODID, name = NAME, version = VERSION)
+public class TutorialMod {
+	
+	@Instance
+	private static TutorialMod instance;
+	
+	public static TutorialMod getInstance() {
+		return instance;
+	}
+	
+	@SidedProxy(serverSide = COMMONPROXY, clientSide = CLIENTPROXY)
+	private static CommonProxy proxy;
+	
+	@EventHandler
+	public void preinit(FMLPreInitializationEvent event) {
+		proxy.preinit(event);
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		proxy.init(event);
+	}
+	
+	@EventHandler
+	public void postinit(FMLPostInitializationEvent event) {
+		proxy.postinit(event);
+	}
+	
+}
