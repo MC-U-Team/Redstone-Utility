e364c5261e79eccff33645b4c6ad6df3457d6854
LucZZ <lucas.weiss1999@web.de>
2018-06-22 00:28:15 +0200

    added D-FlipFlop (WIP)

--- src/main/java/info/u_team/redstone_utility/init/RedstoneUtilityBlocks.java
+++ src/main/java/info/u_team/redstone_utility/init/RedstoneUtilityBlocks.java
@@ -26,6 +26,7 @@ public class RedstoneUtilityBlocks {
 	public static final BlockXorGate blockxorgate = new BlockXorGate();
 	public static final BlockTFlipflopGate blocktflipflopgate = new BlockTFlipflopGate();
 	public static final BlockRSNORLatchGate rsnorlatchgate = new BlockRSNORLatchGate();
+	public static final BlockDFlipflopGate dflipflop = new BlockDFlipflopGate();
 	
 	@Mod.EventBusSubscriber(modid = RedstoneUtilityConstants.MODID)
 	public static class Registry {
