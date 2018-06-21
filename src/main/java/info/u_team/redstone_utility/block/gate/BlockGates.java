36bd8ab9f853908ea4521c2d43f1059ddddfd562
HyCraftHD <hycrafthd@live.de>
2018-06-21 11:56:13 +0200

    BlockGates is now with Material circuits

--- src/main/java/info/u_team/redstone_utility/block/gate/BlockGates.java
+++ src/main/java/info/u_team/redstone_utility/block/gate/BlockGates.java
@@ -14,39 +14,38 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockGates extends Block {
-
+	
 	private static final AxisAlignedBB REDSTONE_DIODE_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.125D, 1.0D);
-
+	
 	public BlockGates() {
-		super(Material.ROCK);
+		super(Material.CIRCUITS);
 		setCreativeTab(TAB);
 	}
-
+	
 	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
 		return REDSTONE_DIODE_AABB;
 	}
-
+	
 	public boolean isFullCube(IBlockState state) {
 		return false;
 	}
-
+	
 	@SideOnly(Side.CLIENT)
-	public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos,
-			EnumFacing side) {
+	public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
 		return side.getAxis() != EnumFacing.Axis.Y;
 	}
-
+	
 	public boolean isOpaqueCube(IBlockState state) {
 		return false;
 	}
-
+	
 	@SideOnly(Side.CLIENT)
 	public BlockRenderLayer getBlockLayer() {
 		return BlockRenderLayer.CUTOUT;
 	}
-
+	
 	public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
 		return face == EnumFacing.DOWN ? BlockFaceShape.SOLID : BlockFaceShape.UNDEFINED;
 	}
-
+	
 }
